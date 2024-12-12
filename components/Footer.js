03083e0cb337157208e111b40c12b1adb30c8a1b
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-12-11 23:40:46 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className=" w-full p-4 bg-black border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 ">
       <span className="text-sm text-gray-300 sm:text-center ">
-        © 2024 Menahil Qasim. Powered by <Link href="https://thestockit.net/" className="underline text-[#4ad1f2]">The Stockit</Link>. All Rights Reserved.
+        © 2024 Menahil Qasim. Developed by <Link href="https://www.linkedin.com/in/aleezarubab/" className="underline text-[#4ad1f2]">Aleeza R.</Link>. All Rights Reserved.
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white  sm:mt-0">
         <li>
